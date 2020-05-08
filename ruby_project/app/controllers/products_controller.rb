1642deca37778e374c9c734c71a30c7f063cb770
Marina00Matta <marinamedhat_19@hotmail.com>
2020-05-08 13:35:18 +0200

    creats products

--- ruby_project/app/controllers/products_controller.rb
+++ ruby_project/app/controllers/products_controller.rb
@@ -13,16 +13,21 @@ class ProductsController < ApplicationController
 
     def create
         @product = Product.new(product_params)
-    
+        @product.user_id = current_user.id
         if @product.save
         redirect_to @product
         else
+        load_data    
         render 'new'
         end
     end
 
     def product_params
-        params.require(:product).permit(:title, :description, :price, :image, :inStock_amount, :category_id, :brand_id)
+        params.require(:product).permit(:title, :description, :price, 
+                            :inStock_amount, :category_id, :brand_id)
+    end    
 
+    def load_data
+        @sellers = User.where(role: 'seller')
     end    
 end    
--- ruby_project/app/models/product.rb
+++ ruby_project/app/models/product.rb
@@ -2,7 +2,8 @@ class Product < ApplicationRecord
 	belongs_to :brand
 	belongs_to :category
 	belongs_to :user
-    has_many :checkouts , dependent: :destroy
+	has_many :checkouts , dependent: :destroy
+	has_one_attached :image
 	validates :title, presence: true
 	validates :description, presence: true
 	validates :price, presence: true  ,:numericality => { greater_than_or_equal_to: 0 }
