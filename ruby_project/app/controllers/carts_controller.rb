e173f8036aad0463ebca0e634444972ff5edd9eb
hager <hgrreda@gmail.com>
2020-05-11 20:31:59 +0200

    instock check

--- ruby_project/app/controllers/carts_controller.rb
+++ ruby_project/app/controllers/carts_controller.rb
@@ -6,23 +6,40 @@ class CartsController < ApplicationController
         
     end
 
+    def check_instock
+      @product = Product.find(params[:id])
+      @instock = @product.inStock_amount
+      
+      # render 'add_to_cart'
+    end
+
     def add_to_cart
         @product = Product.find(params[:id])
+        session[:product_id] = @product.id
         @cart = Checkout.where("cart_id = ? AND product_id = ?", session[:cart_id], @product.id).first
         if @cart
+          if @product.inStock_amount >= 1
            @cart.quantity = @cart.quantity + 1
            @cart.save
+           @product.inStock_amount -=1
+           @product.save
+          end
         else
         @cart = Checkout.new
         @cart.cart_id = session[:cart_id]
         @cart.order_id = session[:order_id]
         @cart.product_id = @product.id
-        @cart.quantity =  1
+        if @product.inStock_amount >= 1
+          @cart.quantity =  1
+          @product.inStock_amount -=1
+          @product.save
+        end
         @cart.user_id = @product.user_id
         @cart.save
         end
-        redirect_to @product
-    end
+        redirect_to request.referrer
+        # redirect_to @product
+      end
 
   def current_cart
     if session[:cart_id]
