9de6b0f878d3c6434cd8c60ea23a9d30a08965d3
Marina00Matta <marinamedhat_19@hotmail.com>
2020-05-11 02:39:09 +0200

    Cart Index view

--- ruby_project/app/controllers/carts_controller.rb
+++ ruby_project/app/controllers/carts_controller.rb
@@ -1,7 +1,11 @@
 class CartsController < ApplicationController
     before_action :authenticated?, :current_cart, :current_order
 
-  
+    def index
+        @carts = Cart.all
+        @current_cart = session[:cart_id]
+        
+    end
 
     def add_to_cart
         @product = Product.find(params[:id])
@@ -9,11 +13,7 @@ class CartsController < ApplicationController
         @cart.cart_id = session[:cart_id]
         @cart.order_id = session[:order_id]
         @cart.product_id = @product.id
-        @cart.quantity = 1
-        @cart.user_id = @product.user_id
         @cart.save
-        render 'add_to_cart'
-
     end
 
   def current_cart
@@ -35,13 +35,12 @@ class CartsController < ApplicationController
       @order.user_id = current_user.id
       @order.state = 'created'
       @order.save
-      session[:order_id] = @order.id
+      session[:cart_id] = @order.id
     end
   end
 
 
   def authenticated?
-    
     if current_user
         session[:user_id] = current_user.id
     else 
@@ -49,5 +48,5 @@ class CartsController < ApplicationController
     end
   end
 
-
+  
 end
